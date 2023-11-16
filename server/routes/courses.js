24c646ecff573bfa3438ade421825b251579ecbc
Leshen <kacper.kamm@gmail.com>
2023-11-16 16:31:39 +0100

    swagger is POG

--- server/routes/courses.js
+++ server/routes/courses.js
@@ -4,5 +4,6 @@ import{getCourse, getStudent} from "../dataGen/generator.js";
 export const coursesRouter = express.Router()
 
 coursesRouter.get("/:id"),(req,res) => {
+    // #swagger.tags = ['Courses']
     res.send(getCourse(req.params.id));
 }
